e3bd7861d8125816c02bc88236f80153e69572d6
pgordon-moric <pgordon@moric.org>
2026-08-06 15:12:30 -0400

    added some logic for avItems

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,50 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "./public/projectors.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "./public/speakers.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "./public/microphones.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "./public/whiteboards.jpg",
+        name: "Whitesboards",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: "./public/signage.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
   ],
-
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if(item){
+            item.quantity++;
+        }      
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0){
+            item.quantity--;
+        }
     },
   },
 });
